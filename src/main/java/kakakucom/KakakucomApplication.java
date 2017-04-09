7a39adc029efd8c4fadda6d5bd538badffea5b91
小川悠貴 <ogawayuuki@ogawayuukinoMacBook-Pro.local>
2017-04-09 10:08:14 +0900

    一式あげる

---
+++ src/main/java/kakakucom/KakakucomApplication.java
@@ -0,0 +1,12 @@
+package kakakucom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakakucomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakakucomApplication.class, args);
+	}
+}
