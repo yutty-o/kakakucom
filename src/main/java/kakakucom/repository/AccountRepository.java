8dac77384446e9f5dd126a4919df7fdd6f681f6f
yutty-o <yuki.ogawa.self-reliance@ezweb.ne.jp>
2017-04-22 15:35:18 +0900

    [fix] ついでにコメント修正

--- src/main/java/kakakucom/repository/AccountRepository.java
+++ src/main/java/kakakucom/repository/AccountRepository.java
@@ -6,12 +6,10 @@ import org.apache.ibatis.annotations.Param;
 
 /**
  * 会員登録レポジトリ
- * Created by yutty on 2017/04/10
+ * Created by ogawayuuki on 2017/04/10
  */
 @Mapper
 public interface AccountRepository {
-
-//    List<User> findOne(@Param("email") final String email);
 
     int create(
         @Param("user") final User user
