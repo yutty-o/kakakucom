0f9001c4a5d1044213b411a9fcce3cb84504948c
yutty-o <yuki.ogawa.self-reliance@ezweb.ne.jp>
2017-05-05 12:18:32 +0900

    [add] 小カテゴリーページのController作成（Controller名をリネーム）

---
+++ src/main/java/kakakucom/app/category/CategoryController.java
@@ -0,0 +1,18 @@
+Unstaged changes after reset:
+M	src/main/java/kakakucom/AppConfig.java
+M	src/main/java/kakakucom/app/IndexController.java
+M	src/main/java/kakakucom/app/category/CategoryController.java
+D	src/main/java/kakakucom/app/category/LargeCategoryController.java
+M	src/main/java/kakakucom/component/exception/ExceptionProvider.java
+M	src/main/java/kakakucom/repository/CategoryRepository.java
+M	src/main/java/kakakucom/service/CategoryService.java
+M	src/main/java/kakakucom/service/ItemService.java
+M	src/main/java/kakakucom/validation/Confirm.java
+M	src/main/java/kakakucom/validation/ConfirmValidator.java
+M	src/main/java/kakakucom/validation/LargeCategoryCd.java
+M	src/main/java/kakakucom/validation/LargeCategoryCdValidator.java
+M	src/main/java/kakakucom/validation/SmallCategoryCd.java
+M	src/main/java/kakakucom/validation/SmallCategoryCdValidator.java
+M	src/main/resources/repository/category/category.xml
+M	src/main/resources/repository/item/item.xml
+M	src/main/resources/templates/index.html
