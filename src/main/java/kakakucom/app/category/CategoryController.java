7a3e6e3b25f5b70fba90832c7ddd47128d82ae60
yutty-o <yuki.ogawa.self-reliance@ezweb.ne.jp>
2017-05-05 12:54:10 +0900

    [fix] CategoryControllerが変なことになってたので修正

--- src/main/java/kakakucom/app/category/CategoryController.java
+++ src/main/java/kakakucom/app/category/CategoryController.java
@@ -1,18 +1,63 @@
-Unstaged changes after reset:
-M	src/main/java/kakakucom/AppConfig.java
-M	src/main/java/kakakucom/app/IndexController.java
-M	src/main/java/kakakucom/app/category/CategoryController.java
-D	src/main/java/kakakucom/app/category/LargeCategoryController.java
-M	src/main/java/kakakucom/component/exception/ExceptionProvider.java
-M	src/main/java/kakakucom/repository/CategoryRepository.java
-M	src/main/java/kakakucom/service/CategoryService.java
-M	src/main/java/kakakucom/service/ItemService.java
-M	src/main/java/kakakucom/validation/Confirm.java
-M	src/main/java/kakakucom/validation/ConfirmValidator.java
-M	src/main/java/kakakucom/validation/LargeCategoryCd.java
-M	src/main/java/kakakucom/validation/LargeCategoryCdValidator.java
-M	src/main/java/kakakucom/validation/SmallCategoryCd.java
-M	src/main/java/kakakucom/validation/SmallCategoryCdValidator.java
-M	src/main/resources/repository/category/category.xml
-M	src/main/resources/repository/item/item.xml
-M	src/main/resources/templates/index.html
+package kakakucom.app.category;
+
+import kakakucom.dto.CategoryDto;
+import kakakucom.model.Item;
+import kakakucom.service.CategoryService;
+import kakakucom.service.ItemService;
+import kakakucom.validation.LargeCategoryCd;
+import kakakucom.validation.SmallCategoryCd;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 商品カテゴリーページ
+ */
+@Controller
+@Validated
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    ItemService itemService;
+
+    /**
+     * 大カテゴリーページ
+     */
+    @RequestMapping(value = "/{largeCategoryCd}")
+    public String showLargeCategory(
+        @PathVariable("largeCategoryCd") @LargeCategoryCd final String largeCategoryCd,
+        Model model
+    ) {
+        CategoryDto category = categoryService.fetchOneByLargeCd(largeCategoryCd);
+        List<Item>  items = itemService.fetchTopItemsByLargeCategoryCd(largeCategoryCd);
+        model.addAttribute("category", category);
+        model.addAttribute("items", items);
+        return "category/largeCategory";
+    }
+
+    /**
+     * 小カテゴリーページ
+     */
+    @RequestMapping(value = "/{largeCategoryCd}/{smallCategoryCd}")
+    public String showSmallCategory(
+        @PathVariable("largeCategoryCd") @LargeCategoryCd final String largeCategoryCd,
+        @PathVariable("smallCategoryCd") @SmallCategoryCd final String smallCategoryCd,
+        Model model
+    ) {
+        CategoryDto category = categoryService.fetchOneByLargeCd(largeCategoryCd);
+        List<Item> items = itemService.fetchTopItemsBySmallCategoryCd(smallCategoryCd);
+        model.addAttribute("category", category);
+        model.addAttribute("items", items);
+        model.addAttribute("selectedSmallCategoryCd", smallCategoryCd);
+        return "category/smallCategory";
+    }
+}
