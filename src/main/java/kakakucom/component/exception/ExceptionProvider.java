0cf29dd6ded24ed6a625d7b39f205aa1c9f08b05
yutty-o <yuki.ogawa.self-reliance@ezweb.ne.jp>
2017-05-05 12:20:39 +0900

    [fix] @Componetアノテーション使えし

--- src/main/java/kakakucom/component/exception/ExceptionProvider.java
+++ src/main/java/kakakucom/component/exception/ExceptionProvider.java
@@ -1,8 +1,11 @@
 package kakakucom.component.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
 import javax.annotation.Nonnull;
 
+@Component
 public class ExceptionProvider {
 
     /**
